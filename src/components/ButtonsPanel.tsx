62ff144dfd174db19d99fbc42465cd2000cfebd3
Elya <elvira.kisling@gmail.com>
2022-02-19 14:48:16 +0300

    store and reducer created

--- src/components/ButtonsPanel.tsx
+++ src/components/ButtonsPanel.tsx
@@ -1,32 +1,37 @@
 import {Button, Col, Row} from "react-bootstrap";
 import React from "react";
+import {useDispatch} from "react-redux";
+import {setButton} from "../redux/calc-reducer";
 
 export const ButtonsPanel = () => {
+
+    const dispatch = useDispatch()
+
     return (
         <>
             <Row className="justify-content-md-center" xs="auto">
-                <Col><Button value= "1" size="lg" variant="outline-primary">1</Button>{' '}</Col>
-                <Col> <Button value= "2" size="lg" variant="outline-primary">2</Button>{' '}</Col>
-                <Col> <Button value= "3" size="lg" variant="outline-primary">3</Button>{' '}</Col>
-                <Col> <Button value= "+" size="lg" variant="outline-primary">+</Button>{' '}</Col>
+                <Col><Button onClick={()=>{dispatch(setButton("1"))}} size="lg" variant="outline-primary">1</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("2"))}} size="lg" variant="outline-primary">2</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("3"))}} size="lg" variant="outline-primary">3</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("+"))}} size="lg" variant="outline-primary">+</Button>{' '}</Col>
             </Row>
             <Row className="justify-content-md-center" xs="auto">
-                <Col ><Button value= "4" size="lg" variant="outline-primary">4</Button>{' '}</Col>
-                <Col> <Button value= "5" size="lg" variant="outline-primary">5</Button>{' '}</Col>
-                <Col> <Button value= "6" size="lg" variant="outline-primary">6</Button>{' '}</Col>
-                <Col> <Button value= "-" size="lg" variant="outline-primary">-</Button>{' '}</Col>
+                <Col ><Button onClick={()=>{dispatch(setButton("4"))}} size="lg" variant="outline-primary">4</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("5"))}} size="lg" variant="outline-primary">5</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("6"))}} size="lg" variant="outline-primary">6</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("-"))}} size="lg" variant="outline-primary">-</Button>{' '}</Col>
             </Row>
             <Row className="justify-content-md-center" xs="auto">
-                <Col><Button value= "7" size="lg" variant="outline-primary">7</Button>{' '}</Col>
-                <Col> <Button value= "8" size="lg" variant="outline-primary">8</Button>{' '}</Col>
-                <Col> <Button value= "9" size="lg" variant="outline-primary">9</Button>{' '}</Col>
-                <Col> <Button value= "*" size="lg" variant="outline-primary">x</Button>{' '}</Col>
+                <Col><Button onClick={()=>{dispatch(setButton("7"))}} size="lg" variant="outline-primary">7</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("8"))}} size="lg" variant="outline-primary">8</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("9"))}} size="lg" variant="outline-primary">9</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("*"))}} size="lg" variant="outline-primary">x</Button>{' '}</Col>
             </Row>
             <Row className="justify-content-md-center" xs="auto">
                 <Col><Button size="lg" variant="outline-primary">CE</Button>{' '}</Col>
-                <Col> <Button size="lg" variant="outline-primary">0</Button>{' '}</Col>
-                <Col> <Button size="lg" variant="outline-primary">.</Button>{' '}</Col>
-                <Col> <Button size="lg" variant="outline-primary">/</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("0"))}} size="lg" variant="outline-primary">0</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("."))}} size="lg" variant="outline-primary">.</Button>{' '}</Col>
+                <Col> <Button onClick={()=>{dispatch(setButton("/"))}}size="lg" variant="outline-primary">/</Button>{' '}</Col>
             </Row>
             <Row className="justify-content-md-center">
                 <Col  xs sm="5">
---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import {combineReducers, createStore} from "redux";
+import {calcReducer} from "./calc-reducer";
+
+const rootReducer = combineReducers({
+     calc: calcReducer
+})
+
+
+export const store = createStore(rootReducer)
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
